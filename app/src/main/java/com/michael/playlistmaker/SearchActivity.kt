c6dad4f6d504b729c6828589f6c78655bcb4ccc3
Mikhail Solodnikov <Sapog730@yandex.ru>
2026-03-17 08:47:46 +0300

    Спринт №9

--- app/src/main/java/com/michael/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/michael/playlistmaker/SearchActivity.kt
@@ -1,6 +1,13 @@
 package com.michael.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +15,23 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.appbar.MaterialToolbar
 
 class SearchActivity : AppCompatActivity() {
+    private var searchText:String = ""
+
+    companion object {
+        const val SEARCH_TEXT = "SEARCH_TEXT"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT, searchText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchText = savedInstanceState.getString(SEARCH_TEXT, searchText)
+        val searchLine = findViewById<EditText>(R.id.search_line)
+        searchLine.setText(searchText)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,8 +42,50 @@ class SearchActivity : AppCompatActivity() {
             insets
         }
         val backButton = findViewById<MaterialToolbar>(R.id.tool_bar)
+        val cancelText = findViewById<TextView>(R.id.clear)
+        val searchLine = findViewById<EditText>(R.id.search_line)
+
         backButton.setNavigationOnClickListener{
             finish()
         }
+
+        cancelText.setOnClickListener{
+            searchLine.setText("")
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(searchLine.windowToken, 0)
+        }
+
+        val textWatcherForSearch = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                cancelText.visibility = clearButtonVisibility(s)
+                searchText=s.toString()
+
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        }
+
+        searchLine.addTextChangedListener(textWatcherForSearch)
     }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+
+
+
 }
