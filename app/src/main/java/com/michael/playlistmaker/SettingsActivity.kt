fa1c3619f8c60c0c28ef581469508b13e207b876
Mikhail Solodnikov <Sapog730@yandex.ru>
2026-05-07 21:44:25 +0300

    2

--- app/src/main/java/com/michael/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/michael/playlistmaker/SettingsActivity.kt
@@ -89,33 +89,3 @@ class SettingsActivity : AppCompatActivity() {
     }
 
 }
-
-class App:Application(){
-
-
-    var darkTheme = false
-    lateinit var sharedPref:SharedPreferences
-
-    override fun onCreate() {
-        super.onCreate()
-        sharedPref = getSharedPreferences(THEME_PREFERENCES, MODE_PRIVATE)
-        darkTheme = sharedPref.getBoolean(EDIT_THEME_KEY, true)
-        switchTheme(darkTheme)
-    }
-
-    fun switchTheme(darkThemeEnabled: Boolean) {
-        darkTheme = darkThemeEnabled
-
-        sharedPref.edit()
-            .putBoolean(EDIT_THEME_KEY, darkTheme)
-            .apply()
-
-        AppCompatDelegate.setDefaultNightMode(
-            if (darkThemeEnabled) {
-                AppCompatDelegate.MODE_NIGHT_YES
-            } else {
-                AppCompatDelegate.MODE_NIGHT_NO
-            }
-        )
-    }
-}
